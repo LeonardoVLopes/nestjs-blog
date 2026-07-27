eb0dbc6fdb7e0ddb3bc43f149e5b5607e7817a4e
leonardo <leonardolopes565@gmail.com>
2026-07-27 20:31:50 -0300

    Injetando o AuthService no AuthController e testando a rota

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService() {
-
+export class AuthService {
+  login() {
+    return 'ola do AuthService';
+  }
 }
