c87dd44f81efca07eeb312ffd18f38eefb931db1
leonardo <leonardolopes565@gmail.com>
2026-08-02 15:59:26 -0300

    ValidationPipe e class-validator para validação dos dados da request no NestJS

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class LoginDto {
-  email: string;
-  password: string;
+  @IsEmail({}, { message: 'E-mail invalido' })
+  email!: string;
+
+  @IsString({ message: 'Senha precisa ser uma string' })
+  @IsNotEmpty({ message: 'Senha nao pode estar vazia' })
+  password!: string;
 }
