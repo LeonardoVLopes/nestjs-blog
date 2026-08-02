1ac503fbc0bb406beaf24dddf782321e1fc6f748
leonardo <leonardolopes565@gmail.com>
2026-08-02 16:22:44 -0300

    Transformando e validando parâmetros com Pipes + Customizando Pipes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { CustomParseIntPipe } from 'src/common/pipes/custom-parse0int-pipe.pipe';
 
 @Controller('user')
 export class UserController {
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', CustomParseIntPipe) id: number) {
     return 'ola do controller do user';
   }
 }
