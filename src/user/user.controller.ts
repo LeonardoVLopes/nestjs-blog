45ad6c1f5610b6cd4ef925cdc3985604dae3e1f9
leonardo <leonardolopes565@gmail.com>
2026-08-23 18:39:53 -0300

    Protegendo rotas com JwtAuthGuard personalizado (JWT + NestJS)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,13 +5,15 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  Req,
   UseGuards,
 } from '@nestjs/common';
 import { CustomParseIntPipe } from 'src/common/pipes/custom-parse0int-pipe.pipe';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 import { ConfigService } from '@nestjs/config';
-import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { AuthenticatedRequest } from 'src/auth/types/authenticated-request';
 
 @Controller('user')
 export class UserController {
@@ -20,10 +22,14 @@ export class UserController {
     private readonly userService: UserService,
   ) {}
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  findOne(@Param('id', CustomParseIntPipe) id: number) {
-    return 'ola do controller do user';
+  findOne(
+    @Req() req: AuthenticatedRequest,
+    @Param('id', CustomParseIntPipe) id: number,
+  ) {
+    console.log(req.user.id);
+    return `ola do controller do user #${id}`;
   }
 
   @Post()
