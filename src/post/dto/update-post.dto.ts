e0b68b506615cf8a992e0d847973c5948ffd7eda
leonardo <leonardolopes565@gmail.com>
2026-09-10 22:45:05 -0300

    DTOs do Post para validação - CreatePostDto, UpdatePostDto e PostResponseDto

--- src/post/dto/update-post.dto.ts
+++ src/post/dto/update-post.dto.ts
@@ -1,4 +1,11 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType, PickType } from '@nestjs/mapped-types';
 import { CreatePostDto } from './create-post.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto extends PartialType(
+  PickType(CreatePostDto, ['title', 'excerpt', 'content', 'coverImageUrl']),
+) {
+  @IsOptional() // vai depender da logica que criamos no service ou no NextJS
+  @IsBoolean({ message: 'Campo de publicar post precisar ser boolean' })
+  published?: boolean;
+}
