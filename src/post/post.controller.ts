214f399d465bc9bd00450751b7e0d669a871a7de
leonardo <leonardolopes565@gmail.com>
2026-09-15 23:40:04 -0300

    Criando findAllOwned e findOneOwned Post para ver os posts do usuário logado

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -8,6 +8,7 @@ import {
   UseGuards,
   Post,
   Req,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { PostService } from './post.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
@@ -24,5 +25,22 @@ export class PostController {
   async create(@Req() req: AuthenticatedRequest, @Body() dto: CreatePostDto) {
     const post = await this.postService.create(dto, req.user);
     return new PostResponseDto(post);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me/:id')
+  async findOneOwned(
+    @Req() req: AuthenticatedRequest,
+    @Param('id', ParseUUIDPipe) id: string,
+  ) {
+    const post = await this.postService.findOneOwnedOrFail({ id }, req.user);
+    return new PostResponseDto(post);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async findAllOwned(@Req() req: AuthenticatedRequest) {
+    const posts = await this.postService.findAllOwned(req.user);
+    return posts.map((post) => new PostResponseDto(post));
   }
 }
